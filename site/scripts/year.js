febbe2dfa2567c4310718bebc1f645cd9be308c4
natalietchi <69836769+natalietchi@users.noreply.github.com>
2024-06-24 02:54:08 +1000

    custom font, wip cookie page, header automation

--- site/scripts/year.js
+++ site/scripts/year.js
@@ -13,4 +13,8 @@ const icons = [
     "<a href='https://rb3dx.milohax.org'><img src='/88x31/rb3dx.gif'></a>"
 ];
 
+const header = [
+    '<img src="../img/header.png"><div class="nav"><b><li><a href="../index.html">[home]</a></li><li><a href="../index.html">[about me]</a></li><li><a href="../index.html">[characters]</a></li><li><a href="../index.html">[blog]</a></li><li><a href="../index.html">[downloads]</a></li></b></div>'
+];
 document.getElementsByClassName("footer")[0].innerHTML ="<p><b>natalietchi</b> " + span + "</p>" + icons;
+document.getElementsByClassName("header")[0].innerHTML = header;
